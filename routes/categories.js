e187528521eddffd5a2c90ba36eb6e4711cfaec6
mhantich <mohame.mhantich6@gmail.com>
2025-01-09 15:33:48 +0000

    Increase cache duration for categories and meals routes from 5 minutes to 10 hours to enhance performance and reduce server load.

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getCategories, createCategory, updateCategory, deleteCategory } = require('../Controller/categories');
 const { auth, authAdmin } = require('../middleware/auth');
 const cache = require('../middleware/cache');
-router.get('/', cache(300), getCategories);
+router.get('/', cache(36000), getCategories);
 router.post('/', auth, authAdmin, createCategory);
 router.put('/:id', auth, authAdmin, updateCategory);
 router.delete('/:id', auth, authAdmin, deleteCategory);
--- routes/meals.js
+++ routes/meals.js
@@ -4,7 +4,7 @@ const Meal = require('../models/Meal');
 const { getMeals } = require('../Controller/meals');
 const cache = require('../middleware/cache');
 // Get all meals
-router.get('/', cache(300), getMeals );
+router.get('/', cache(36000), getMeals );
 // router.post('/',createMeal);
 // router.put('/:id',updateMeal);
 // router.delete('/:id',deleteMeal);
